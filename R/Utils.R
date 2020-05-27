9aaa54d986026c0f76f2524a7f7e83de1d3dc74e
Angelo D'Ambrosio <bakaburg1@gmail.com>
2020-05-27 22:30:00 +0200

    - Added logit, invlogit, my.round, sumfun

--- R/Utils.R
+++ R/Utils.R
@@ -1,8 +1,30 @@
 percent <- function(x) {
 	#sapply(x, function(x) if (!is.na(x)) {if (abs(x * 100) < 1) sprintf('%.2g%%', x * 100) else sprintf('%.3g%%', x * 100)} else NA)
-	sapply(x, function(x) if (!is.na(x)) {if (abs(x * 100) < 1) glue('{signif(x * 100, 2)}%') else glue('{signif(x * 100, 3)}%')} else NA)
+	sapply(x, function(x) if (!is.na(x)) {if (abs(x * 100) < 1) paste0(signif(x * 100, 2), '%') else paste0(signif(x * 100, 3), '%')} else NA)
 }
 
 '%nin%' <- function(x, y) {
 	!(x %in% y)
+}
+
+invlogit <- function(x)
+{
+	1/(1 + exp(-x))
+}
+
+logit <- function(x)
+{
+	log(x/(1 - x))
+}
+
+my.round <- function(z) {
+	if_else(z < 1, signif(z, 2), round(z))
+}
+
+
+sumfun <- function(x, level = .5) {
+
+	#, mode: {names(table(x))[which.max(table(x))]}
+	sprintf('%s, %s%%CI: [%s, %s]', my.round(median(x)), 100 * level, my.round(quantile(x, .5 - level/2)), my.round(quantile(x, .5 + level/2))) %>%
+		stringr::str_replace_all('0\\.\\d+', function(x) percent(as.numeric(x)))
 }
