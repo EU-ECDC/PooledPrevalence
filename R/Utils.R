e2d5464ed3e1090f1876b215603129bbab9bea17
Angelo D'Ambrosio <bakaburg1@gmail.com>
2020-06-02 19:52:26 +0200

    - Added function to compile the stan programs - Fixed function name to respect underscore style

--- R/Utils.R
+++ R/Utils.R
@@ -17,14 +17,18 @@ logit <- function(x)
 	log(x/(1 - x))
 }
 
-my.round <- function(z) {
-	if_else(z < 1, signif(z, 2), round(z))
+my_round <- function(z) {
+	dplyr::if_else(z < 1, signif(z, 2), round(z))
 }
 
 
 sumfun <- function(x, level = .5) {
 
 	#, mode: {names(table(x))[which.max(table(x))]}
-	sprintf('%s, %s%%CI: [%s, %s]', my.round(median(x)), 100 * level, my.round(quantile(x, .5 - level/2)), my.round(quantile(x, .5 + level/2))) %>%
+	sprintf('%s, %s%%CI: [%s, %s]', my_round(median(x)), 100 * level, my_round(quantile(x, .5 - level/2)), my_round(quantile(x, .5 + level/2))) %>%
 		stringr::str_replace_all('0\\.\\d+', function(x) percent(as.numeric(x)))
+}
+
+compile.estimation.model <- function() {
+	invisible(rstan::stan_model('Estimation_model.stan', save_dso = T, auto_write = T))
 }
